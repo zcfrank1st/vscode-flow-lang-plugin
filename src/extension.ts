37314b96593f9486c64f62feca819a774b0c9128
zcfrank1st <zhchaos@gmail.com>
2020-01-08 07:40:59 +0800

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { exec } from 'child_process';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+export function activate(context: vscode.ExtensionContext) {
+	const flowPath = vscode.workspace.getConfiguration("flowchart");
+	const editor = vscode.window.activeTextEditor;
+
+	if (flowPath && editor) {
+		let disposable = vscode.commands.registerTextEditorCommand('extension.flowchart', () => {
+			let documentText = editor.document.getText();
+			
+			const panel = vscode.window.createWebviewPanel(
+				'flowChart',
+				'flowchart',
+				vscode.ViewColumn.Beside,
+				{
+					enableScripts: true,
+					retainContextWhenHidden: true
+				}
+			)
+
+			exec(`java -Dtext="${documentText}" -jar ${flowPath.get("path")}`, (error, stdout, stderr) =>{
+				if (!error) {
+					panel.webview.html = stdout;
+				} else {
+					vscode.window.showInformationMessage(stderr);
+				}
+			});
+		});
+	
+		context.subscriptions.push(disposable);	
+	}
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
